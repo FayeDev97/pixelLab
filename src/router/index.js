b94e49b2dd8fd1c65718c3c8dd7a84fd90236e67
FayeDev97 <momofaye51@gmail.com>
2022-03-27 18:27:22 +0000

    Revert "second draft"
    This reverts commit 563dc659d4f1e4df745c3ccdc26ddc2fc5dd43d9.

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import EditorView from "../views/EditorView.vue";
+import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: EditorView,
+    component: HomeView,
   },
   {
     path: "/about",
