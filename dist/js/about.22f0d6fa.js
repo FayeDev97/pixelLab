ef427f5cd438feebc5be3bac53b4d7694cc33607
FayeDev97 <momofaye51@gmail.com>
2022-04-14 00:28:01 +0000

    Include dist folder
    Include dist folder for gitpage

---
+++ dist/js/about.22f0d6fa.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpixel_doodles"]=self["webpackChunkpixel_doodles"]||[]).push([[443],{7381:function(e,n,s){s.r(n),s.d(n,{default:function(){return d}});var u=s(3396);const t={class:"about"},a=(0,u._)("h1",null,"This is an about page",-1),o=[a];function r(e,n){return(0,u.wg)(),(0,u.iD)("div",t,o)}var i=s(89);const l={},c=(0,i.Z)(l,[["render",r]]);var d=c}}]);
+//# sourceMappingURL=about.22f0d6fa.js.map
